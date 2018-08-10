c3a4b779edae14469b0728e322ddafcf94c3fde1
PradeepNalluri <pradeep.k@t-hub.co>
2018-08-10 17:19:32 +0530

    median done..

--- main.c
+++ main.c
@@ -1,6 +1,52 @@
 #include<stdio.h>
 #include<math.h>
+void merge(float arr[], int left, int middle, int right){
+    int i, j, k;
+    int n1 = middle - left + 1;
+    int n2 =  right - middle;
+    int L[n1], R[n2];
+    for (i = 0; i < n1; i++)
+        L[i] = arr[left + i];
+    for (j = 0; j < n2; j++)
+        R[j] = arr[middle + 1+ j];
+    i = 0;
+    j = 0;
+    k = left;
+    while (i < n1 && j < n2){
+        if (L[i] <= R[j]){
+            arr[k] = L[i];
+            i++;
+        }
+        else{
+            arr[k] = R[j];
+            j++;
+        }
+        k++;
+    }
+while (i < n1){
+        arr[k] = L[i];
+        i++;
+        k++;
+    }
+    while (j < n2){
+        arr[k] = R[j];
+        j++;
+        k++;
+    }
+}
+void mergeSort(float arr[], int left, int right){
+    if (left < right){
+        int middle = left+(right-left)/2;
+        //recursively breaking array and sorting
+        mergeSort(arr, left, middle);
+        mergeSort(arr, middle+1, right);
+        merge(arr, left, middle, right);
+    }
+}
+
+
 int main(){
+  //Getting Input
   printf("Please Enter the number of numbers you want to enter: ");
   int num_num;
   scanf("%d",&num_num);
@@ -10,6 +56,7 @@ int main(){
     printf("Please enter element number %d: ",i+1);
     scanf("%f",&num_arr[i]);
   }
+  //Calculation of Mean
   float sum=0;
   for(i=0;i<num_num;i++){
     sum+=num_arr[i];
@@ -17,6 +64,7 @@ int main(){
   float mean=sum/num_num;
   printf("Mean(rounded of to 2 digits): %.2f\n",mean);
   sum=0;
+  //Calculation of Standard Deviation
   float tbs;
   for(i=0;i<num_num;i++){
     tbs=mean-num_arr[i];
@@ -26,6 +74,19 @@ int main(){
   float standard_deviation;
   standard_deviation=sum/num_num;
   standard_deviation=sqrt(standard_deviation);
-  printf("Median(rounded of to 2 digits): %.2f\n",standard_deviation);
-
+  printf("Standard Deviation(rounded of to 2 digits): %.2f\n",standard_deviation);
+  //Calculation of Median
+  //Sorting of the array
+  float sorted_arr[num_num];
+  for(i=0;i<num_num;i++){
+    sorted_arr[i]=num_arr[i];
+  }
+  mergeSort(sorted_arr,0,num_num-1);
+  float median;
+  if(num_num%2==0){
+    median=(sorted_arr[num_num/2]+sorted_arr[(num_num/2)-1])/2;
+  }else{
+    median=sorted_arr[num_num/2];
+  }
+  printf("Median: %f\n",median);
 }
