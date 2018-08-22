5b84a2deae63e8ebfc19855420f8b6a9a4f2200b
PradeepNalluri <pradeep.k@t-hub.co>
2018-08-22 11:09:01 +0530

    Blank lies hadled

--- Assignment 2/main.c
+++ Assignment 2/main.c
@@ -12,11 +12,22 @@ int main(int argc, char const *argv[]) {
  }
  char c;
  int count=0;
+ int count2=0;
+ int count3=0;
  while ((c = fgetc(inputf)) != EOF) {
-   if(c=='\n'){
+   if(c=='\n' && count2!=0){
      count+=1;
+     count2=0;
+   }
+   else if(c=='\n' && count2==0){
+     count3+=1;
+   }
+   else{
+     count2+=1;
    }
 }
-  printf("The number of lines in the file are: %d\n",count);
+  printf("The number of lines in the file which are not blank are: %d\n",count);
+  printf("The number of lines in the file which are blank are: %d\n",count3);
+  printf("Total number of lines in the file are: %d\n",count+count3);
   return 0;
 }
